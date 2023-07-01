a65487eb2842bb69437714533480554172fe0232
LeifCastle <leifurcastle@gmail.com>
2023-07-01 00:59:19 -0700

    chore: basic home page creation/style connection

---
+++ src/app/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="flex justify-evenly items-center w-screen h-[8%] bg-Dark2 text-slate-300 rounded-bl-lg rounded-br-lg">
+      <div></div>
+      <h1 className="text-4xl">Night Light</h1>
+      <div>
+        <button>Login</button>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        Dark1: "#062925",
+        Dark2: "#044A42",
+        Light1: "#3A9188",
+        Light2: "#B8E1DD",
       },
     },
   },
   plugins: [],
-}
+};
